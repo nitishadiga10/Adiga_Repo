426605b10eed77416a0e23037bcef4287b57adb5
nitish.adiga10@gmail.com <nitish.adiga10@gmail.com>
2020-01-13 21:57:55 +0530

    deploying to the prod

--- src/app/components/dialog-box/dialog-box.component.ts
+++ src/app/components/dialog-box/dialog-box.component.ts
@@ -9,7 +9,7 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 })
 export class DialogBoxComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) private data) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data) { }
 
   ngOnInit() {
   }
