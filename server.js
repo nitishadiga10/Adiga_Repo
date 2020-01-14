f73dc2e0de431297f21b9ecd6cb36da43af24930
nitish.adiga10@gmail.com <nitish.adiga10@gmail.com>
2020-01-14 11:25:10 +0530

    trying out

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/LoginPoject'));
+app.use(express.static(path.join(__dirname, 'dist/LoginProject')));
 
 app.get('*', function(req,res) {
     
